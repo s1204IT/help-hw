3e3994bba871a9b9657b8afed9ff18ae0e901e7f
Syuugo <pub@s1204.me>
2022-11-12 10:02:19 +0900

    `--force-downgrade`オプションの追加

--- libpopt0-patcher.sh
+++ libpopt0-patcher.sh
@@ -12,7 +12,7 @@ fi
 
 curl -O# http://ftp.jp.debian.org/debian/pool/main/p/popt/libpopt0_1.18-2_amd64.deb
 if [ $? != 0 ]; then echo -e "\nパッケージのダウンロードに失敗しました\n正常なインターネット接続を確認してください\n"; exit 1; fi
-sudo dpkg -i ./libpopt0_1.18-2_amd64.deb
+sudo dpkg --force-downgrade -i ./libpopt0_1.18-2_amd64.deb
 rm -f ./libpopt0_1.18-2_amd64.deb
 sudo apt-mark hold libpopt0
 echo -e "\n\"libpopt0\"の上書きが完了しました｡\n\"help-hw\"を正常にお使い頂けます｡\n"
